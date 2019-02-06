6d4cc3fedbed5e92025e49b3d474a4246315ef1a
Pau Andrio <andriopau@gmail.com>
2019-02-06 11:10:18 +0100

    Feb 2019 Release No Ambertools

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_analysis",
-    version="0.1.0",
+    version="0.1.1",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="biobb_analysis is the Biobb module collection to perform analysis of molecular dynamics simulations.",
