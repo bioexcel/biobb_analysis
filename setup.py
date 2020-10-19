a18fc163b30950fb38d0ca01e417e7b92ddf90a7
gbayarri <genis.bayarri@irbbarcelona.org>
2020-10-19 10:32:42 +0200

    2020.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_analysis",
-    version="3.0.1",
+    version="3.0.2",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="Biobb_analysis is the Biobb module collection to perform analysis of molecular dynamics simulations.",
@@ -18,7 +18,7 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs', 'test']),
-    install_requires=['biobb_common==3.0.0'],
+    install_requires=['biobb_common==3.0.1'],
     python_requires='==3.7.*',
     classifiers=(
         "Development Status :: 3 - Alpha",
