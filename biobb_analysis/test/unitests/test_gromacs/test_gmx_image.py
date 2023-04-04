7d5efabd6c90332019533c7879a2a9154ecd630d
Pau Andrio <andriopau@gmail.com>
2023-04-04 17:15:22 +0200

    assert equal using compare_size

--- biobb_analysis/test/unitests/test_gromacs/test_gmx_image.py
+++ biobb_analysis/test/unitests/test_gromacs/test_gmx_image.py
@@ -1,6 +1,6 @@
 from biobb_common.tools import test_fixtures as fx
 from biobb_analysis.gromacs.gmx_image import gmx_image
-from biobb_analysis.gromacs.common import gmx_check
+# from biobb_analysis.gromacs.common import gmx_check
 # import platform
 
 
@@ -15,7 +15,7 @@ class TestGMXImage():
     def test_image(self):
         gmx_image(properties=self.properties, **self.paths)
         assert fx.not_empty(self.paths['output_traj_path'])
-        print(self.properties.get('binary_path', 'gmx'))
-        assert gmx_check(self.paths['output_traj_path'], self.paths['ref_output_traj_path'], gmx=self.properties.get('binary_path', 'gmx'))
+        # assert gmx_check(self.paths['output_traj_path'], self.paths['ref_output_traj_path'], gmx=self.properties.get('binary_path', 'gmx'))
+        assert fx.equal(self.paths['output_traj_path'], self.paths['ref_output_traj_path'])
         # if platform.system() == 'Darwin':
         #    assert fx.equal(self.paths['output_traj_path'], self.paths['ref_output_traj_path'])
